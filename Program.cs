460181fe99c08e40c5cd00a5535a333bd525288f
Benjamin Rowen <Benjamin.Rowen@safeguardproperties.com>
2022-04-28 13:12:13 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>();
+            
+            // get input from console and assign it to a value
+            while  (true) {
+                Console.WriteLine("Please enter an employee name: ");
+                string input = Console.ReadLine();
+                // Break if user enters an empty string
+                if (input == "") {
+                    break;
+                }
+                employees.Add(input);
+            }
+
+            Console.WriteLine("Your employee list: ");
+            for(int i=0; i<employees.Count; i++) {
+                Console.WriteLine(employees[i]);
+            }
+        }
+    }
+}
