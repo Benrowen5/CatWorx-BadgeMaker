408a88debc600830194c10fb96f9935b987ab1fd
Benjamin Rowen <Benjamin.Rowen@safeguardproperties.com>
2022-04-29 08:55:16 -0400

    Added CLI prompts for employee data, privatized employee class data, added public employee methods to retrieve employee data

--- Employee.cs
+++ Employee.cs
@@ -5,18 +5,28 @@ namespace CatWorx.BadgeMaker
 {
     class Employee 
     {
-        public string FirstName;
-        public string LastName;
-        public int EmployeeId;
-        public string PhotoUrl;
+        private string FirstName;
+        private string LastName;
+        private int EmployeeId;
+        private string PhotoUrl;
 
-        public Employee(string firstName, string lastName) {
+        public Employee(string firstName, string lastName, int employeeId, string photoUrl) {
             FirstName = firstName;
             LastName = lastName;
+            EmployeeId = employeeId;
+            PhotoUrl = photoUrl;
         }
 
         public string GetName() {
             return FirstName + " " + LastName;
+        }
+
+        public int GetId() {
+            return EmployeeId;
+        }
+
+        public string GetPhotoUrl() {
+            return PhotoUrl;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,35 +5,44 @@ namespace CatWorx.BadgeMaker
 {
     class Program
     {
-        static List<string> GetEmployees() 
+        static List<Employee> GetEmployees() 
         {
-            List<string> employees = new List<string>();
+            List<Employee> employees = new List<Employee>();
             while(true) {
-                Console.WriteLine("Please enter a name: (leave empty to exit): ");
-                string input = Console.ReadLine();
-                if (input == "") {
+                // Get user input for employee and save to a new Employee instance
+                Console.WriteLine("Please enter first name (leave empty to exit): ");
+                string firstName = Console.ReadLine();
+                if (firstName == "") {
                     break;
                 }
+                Console.Write("Enter last name: ");
+                string lastName = Console.ReadLine();
+                Console.Write("Enter employee ID number: ");
+                int employeeId = Int32.Parse(Console.ReadLine());
+                Console.Write("Enter employee photo URL: ");
+                string photoUrl = Console.ReadLine();
 
                 // create new Employee instance
-                Employee currentEmployee = new Employee(input, "Smith");
-                employees.Add(currentEmployee.GetName());
+                Employee currentEmployee = new Employee(firstName, lastName, employeeId, photoUrl);
+                // Add currentEmployee of Employee type
+                employees.Add(currentEmployee);
             }
             // important
             return employees;
         }
 
-        static void PrintEmployees(List<string> employees) 
+        static void PrintEmployees(List<Employee> employees) 
         {
             for (int i=0; i < employees.Count; i++) {
-                Console.WriteLine(employees[i]);
+                string template = "{0,-10}\t{1,-20}\t{2}";
+                Console.WriteLine(String.Format(template, employees[i].GetId(), employees[i].GetName(), employees[i].GetPhotoUrl()));
             }
         }
 
         static void Main(string[] args)
         {
             // GetEmployees method call
-            List<string> employees = GetEmployees();
+            List<Employee> employees = GetEmployees();
             
             Console.WriteLine("Your employee list: ");
             // PrintEmployees method call, passing employees in as parameter.
