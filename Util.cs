230459b9e31fe3dee64d8b8522e19a4295533c68
Benjamin Rowen <Benjamin.Rowen@safeguardproperties.com>
2022-05-02 11:15:46 -0400

    Employee badge gets created using employee image.

--- Util.cs
+++ Util.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Drawing;
+using System.Net;
 using System.Collections.Generic;
 
 namespace CatWorx.BadgeMaker
@@ -33,8 +34,45 @@ namespace CatWorx.BadgeMaker
         }
 
         public static void MakeBadges(List<Employee> employees) {
+            // Badge layout variables
+            int BADGE_WIDTH = 669;
+            int BADGE_HEIGHT = 1044;
+
+            int COMPANY_NAME_START_X = 0;
+            int COMPANY_NAME_START_Y = 110;
+            int COMPANY_NAME_WIDTH = 100;
+
+            int PHOTO_START_X = 184;
+            int PHOTO_START_Y = 215;
+            int PHOTO_WIDTH = 302;
+            int PHOTO_HEIGHT = 302;
+
+            int EMPLOYEE_NAME_START_X = 0;
+            int EMPLOYEE_NAME_START_Y = 560;
+            int EMPLOYEE_NAME_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_NAME_HEIGHT = 100;
+
+            int EMPLOYEE_ID_START_X = 0;
+            int EMPLOYEE_ID_START_Y = 690;
+            int EMPLOYEE_ID_WIDTH = BADGE_WIDTH;
+            int EMPLOYEE_ID_HEIGHT = 100;
+
             // create new image
             Image newImage = Image.FromFile("badge.png");
+            newImage.Save("data/employeeBadge.png");
+
+            using(WebClient client = new WebClient()) {
+                for (int i = 0; i < employees.Count; i++) {
+                    Image photo = Image.FromStream(client.OpenRead(employees[i].GetPhotoUrl()));
+                    Image background = Image.FromFile("badge.png");
+                    Image badge = new Bitmap(BADGE_WIDTH, BADGE_HEIGHT);
+                    Graphics graphic = Graphics.FromImage(badge);
+                    graphic.DrawImage(background, new Rectangle(0,0,BADGE_WIDTH, BADGE_HEIGHT));
+                    graphic.DrawImage(photo, new Rectangle(PHOTO_START_X, PHOTO_START_Y, PHOTO_WIDTH, PHOTO_HEIGHT));
+                    // badge.Save("data/employeeBadge.png");               
+                }
+            }
+            
         }
     }
 }
